e669aadf66bdbe4b4e3a5b3cbfe44cdef9e7368f
sofy2831 <s.dumas974@orange.fr>
2025-04-03 19:10:35 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,49 @@
+const CACHE_NAME = 'PWA-Toxdetect-cache';
+const CACHE_URLS = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/styles.css', 
+    '/abon.html',
+    '/audio.html', 
+    '/auth.html',
+    '/conditions.html', 
+    '/confidentialite.html',
+    '/faq.html', 
+    '/infos.html',
+    '/infos.js', 
+    '/journal.html',
+    '/journal.js', 
+    '/parametres.html',
+    '/quiz.html', 
+    '/quiz.js',
+    '/urgences.html',
+    '/urgences.js', 
+    '/video.html',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('PWA-Toxdetect-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/index.js',
-                '/styles.css', 
-                '/abon.html',
-                '/audio.html', 
-                '/auth.html',
-                '/conditions.html', 
-                '/confidentialite.html',
-                '/faq.html', 
-                '/infos.html',
-                '/infos.js', 
-                '/journal.html',
-                '/journal.js', 
-                '/parametres.html',
-                '/quiz.html', 
-                '/quiz.js',
-                '/urgences.html',
-                '/urgences.js', 
-                '/video.html',
-                '/manifest.json',
-                '/icons/icon-192x192.png',
-                '/icons/icon-512x512.png'
-            ]);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(CACHE_URLS);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
@@ -33,7 +51,13 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).then((fetchResponse) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
         })
     );
 });
+
